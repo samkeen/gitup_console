9ef078af634e46247e9c317e0eda2ffdc3b81f6d
samkeen <sam@shopigniter.com>
2012-12-31 23:35:29 -0800

    doc blocks

--- Updater.py
+++ Updater.py
@@ -13,10 +13,34 @@ class Updater:
         self.prompt_user_on = commandline_args.prompt_user
 
     def verbose(self, message, type='normal'):
+        """
+        wrap verbose message output so it can be turned off when not in -v mode
+
+        Parameters
+        ----------
+        message : str
+        type : str (optional) defaults to Console.NORMAL
+            one of Console.NORMAL, Console.WARN, Console.ERROR, Console.SUCCESS
+        Returns
+        ----------
+        list
+        """
         if self.verbose_on:
             Console.out(message, type)
 
     def get_menu(self):
+        """
+        Construct a Selection Menu string
+        i.e.
+          1) ALL
+          2) alley-oop
+
+          Select the Repos to update (space|comma delimited list. i.e. 2 3 7 OR 2, 3, 7): 2
+
+        Returns
+        ----------
+        str The constructed selection menu intended for the commandline
+        """
         menu_list = "  1) ALL\n"
         for index, repo_meta in enumerate(self.settings.KNOWN_REPOS):
             menu_list += "  " + str(index + 2) + ") " + repo_meta['name'] + "\n"
@@ -24,6 +48,13 @@ class Updater:
         return menu_list
 
     def get_menu_input(self):
+        """
+        Capture the user input in response to the menu produced via self.get_menu()
+
+        Returns
+        ----------
+        list
+        """
         input = raw_input("Select the Repos to update (space|comma delimited list. i.e. 2 3 7 OR 2, 3, 7): ")
         # pull the input, trimming out extra space, taking commas into account if present
         input_repos = re.findall(r'\d+', input)
@@ -31,6 +62,18 @@ class Updater:
         return map(lambda x: int(x) - 2, input_repos)
 
     def get_repos_to_process(self, repo_index_input):
+        """
+        for the given list of indexes (), return the correlating items
+        in settings.KNOWN_REPOS
+
+        Parameters
+        ----------
+        repo_index_input : list
+            List of indexes correlating to settings.KNOWN_REPOS
+        Returns
+        ----------
+        list
+        """
         repos_to_clone = []
         for index in repo_index_input:
             if index == -1:
@@ -44,14 +87,18 @@ class Updater:
             self.verbose("The repos to be processed are: " + ", ".join(map(lambda x: x['name'], repos_to_clone)))
         return repos_to_clone
 
-    #
-    #    def get_repo_item_with_index(self, index_number):
-    #        found_repo = None
-    #        for repo_meta in self.settings.KNOWN_REPOS:
-    #            if repo_meta['index'] == index_number
-    #                found_repo =
-
     def command(self, command_to_run):
+        """
+        Issue a system command.  Capture any errors
+
+        Parameters
+        ----------
+        command_to_run : str|list (if str, it is converted to single item list)
+            The settings module for this app
+        Returns
+        ----------
+        str The STDOUT result from the command
+        """
         if type(command_to_run) is str:
             command_to_run = [command_to_run]
         try:
@@ -69,13 +116,39 @@ class Updater:
         return output
 
     def assert_path_exists(self, path, additional_message=''):
+        """
+        Utility method to assert a filesystem path exists
+
+        Parameters
+        ----------
+        path : str
+            The settings module for this app
+        additional_message : str (optional)
+            If given, additional message to output if path does
+            not exist
+        Returns
+        ----------
+        None (exit if path not exists)
+        """
         if not os.path.exists(path):
             Console.out("path [" + path + "] does not exist", Console.ERROR)
             Console.out(additional_message, Console.ERROR)
             sys.exit()
 
     def prep_build(self, settings):
-        # clean
+        """
+        Cleanup from previous runs of app.
+        Create any resources (dirs, etc) for the next build
+
+        Parameters
+        ----------
+        settings : module
+            The settings module for this app
+
+        Returns
+        ----------
+        None
+        """
         self.verbose("Removing build dir at: {}".format(settings.BUILD_DIR))
         if os.path.isdir(settings.BUILD_DIR):
             shutil.rmtree(settings.BUILD_DIR)
@@ -84,6 +157,19 @@ class Updater:
         os.mkdir(settings.BUILD_DIR)
 
     def clone_repo(self, settings, repo_name):
+        """
+        Clone the given git repo into the build directory
+
+        Parameters
+        ----------
+        settings : module
+            The settings module for this app
+        repo_name : string
+
+        Returns
+        ----------
+        None
+        """
         clone_url = settings.GIT_REPO_BASE_CLONE_PATH + "/{0}.git".format(repo_name)
         clone_target_path = "{0}/{1}".format(settings.BUILD_DIR, repo_name)
         git_command_parts = ['git', 'clone', clone_url, clone_target_path]
@@ -93,6 +179,17 @@ class Updater:
         self.verbose(self.command(git_command_parts))
 
     def pull_branch_origin_latest(self, branch_name):
+        """
+        Update to current branch to the latest on origin
+
+        Parameters
+        ----------
+        branch_name : string
+
+        Returns
+        ----------
+        None
+        """
         self.assert_known_branch(branch_name)
         git_pull_command_parts = ['git', 'pull', 'origin', branch_name]
         self.verbose("issuing command:")
@@ -100,6 +197,17 @@ class Updater:
         self.verbose(self.command(git_pull_command_parts))
 
     def assert_known_branch(self, branch_name):
+        """
+        Assert that the given branch name exists for the current repo
+
+        Parameters
+        ----------
+        branch_name : string
+
+        Returns
+        ----------
+        None (exit if branch unknown)
+        """
         branch_list = self.get_branch_list()
         if not any(branch_name in s for s in branch_list):
             Console.out("Branch {0} is an unknown branch.  Known branches are [{1}]".format(
@@ -110,21 +218,43 @@ class Updater:
 
     def get_branch_list(self):
         """
-        :rtype : List
+        get the list of branched for a git repo
+
+        Returns
+        ----------
+        list
         """
         self.command(['git', 'fetch'])
         branch_output = self.command(['git', 'branch'])
         return self.strip_list(branch_output.splitlines(), ' *')
 
     def strip_list(self, l, charset=' '):
+        """
+        utility method to strip whitespace from a list of strings
+
+        Parameters
+        ----------
+        l : list
+        charset : string (optional)
+            string of characters to strip from each item in the list
+        Returns
+        ----------
+        list
+        """
         return [x.strip(charset) for x in l]
 
-    def get_repo_origin_branch_head_sha(self, settings, repo_name, branch_name):
-        self.clone_repo(settings, repo_name)
-        self.chdir_to_repo(repo_name)
-        self.assert_known_branch(branch_name)
-
     def init_submodule(self, submodule_path):
+        """
+        initialize a git submodule
+
+        Parameters
+        ----------
+        submodule_path : str
+            relative path to the submodule directory
+        Returns
+        ----------
+        None
+        """
         self.assert_path_exists(submodule_path, "This is the expected path to the Saccharin submodule")
         init_command_parts = ['git', 'submodule', 'update', '--init', submodule_path]
         self.verbose("initing submodule: {0}".format(submodule_path))
@@ -132,6 +262,17 @@ class Updater:
         self.verbose(self.command(init_command_parts))
 
     def prompt_user_to_continue(self, message="Continue [y/N] "):
+        """
+        Utility method to prompt use to continue or exit
+
+        Parameters
+        ----------
+        message : str (optional)
+            String to use as prompt. defaults to 'Continue [y/N] '
+        Returns
+        ----------
+        None
+        """
         if self.prompt_user_on:
             input = raw_input(message)
             if input.upper() != 'Y':
@@ -139,7 +280,14 @@ class Updater:
                 sys.exit()
 
     def get_current_branch_name(self):
-        # git rev-parse --abbrev-ref HEAD
+        """
+        Get the name of the current git branch
+
+        Returns
+        ----------
+        str
+            The name of the current branch
+        """
         branch_name_command_parts = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
         self.verbose("Determining the current branch name")
         self.verbose("Issuing command: " + " ".join(branch_name_command_parts))
@@ -148,6 +296,18 @@ class Updater:
         return current_branch
 
     def checkout_branch(self, branch_name):
+        """
+        Checkout the given branch (branch_name) for the repo we are currently in
+
+        Parameters
+        ----------
+        branch_name : str
+            The branch name
+        Returns
+        ----------
+        str
+            The sha of the checked out branch
+        """
         self.assert_known_branch(branch_name)
         current_branch_name = self.get_current_branch_name()
         if branch_name == current_branch_name:
@@ -160,6 +320,19 @@ class Updater:
         return self.command(['git', 'rev-parse', 'HEAD']).strip()
 
     def chdir_to_repo(self, settings, repo_name):
+        """
+        Helper method to cd into the given (repo_name) repo dir.
+
+        Parameters
+        ----------
+        settings : module
+            The settings module for this app
+        repo_name : str
+            The repo dir name
+        Returns
+        ----------
+        None
+        """
         repo_path = "{0}/{1}".format(settings.BUILD_DIR, repo_name)
         self.assert_path_exists(repo_path, "This is the expected path to the repo: {0}".format(repo_name))
         self.verbose("Changing to dir: {0}".format(repo_path))
